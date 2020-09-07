4169c7bee6c84e6d929f9da909ce9393db949f55
Thomas <thomas@192.168.2.47>
2020-09-07 06:25:57 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,255 @@
+import numpy as np
+from timeit import default_timer as timer
+import threading
+import queue
+import multiprocessing
+import warnings
+
+from keras.models import load_model
+import keras
+
+from utils import get_hidden_layers
+from utils import get_corr
+from utils import crossover_method
+from utils import arithmetic_crossover
+from utils import load_cifar
+from utils import load_mnist
+from utils import add_noise_to_fittest
+from utils import low_corr_neurons
+
+warnings.filterwarnings("ignore")
+
+
+class CustomSaver(keras.callbacks.Callback):
+    def __init__(self, epoch_list, parent_id):
+        self.epoch_list = epoch_list
+        self.parent_id = parent_id
+
+    def on_epoch_end(self, epoch, logs={}):
+        if epoch in self.epoch_list:
+            self.model.save("model_" + self.parent_id + "_epoch" + str(epoch) + ".hd5")
+
+
+def model_keras(seed, data, weights_hidden_size=None):
+    if data == "mnist":
+        input_size = 784
+        hidden_size = 512
+        output_size = 10
+
+        if weights_hidden_size is not None:
+            hidden_size = weights_hidden_size
+
+        initializer = keras.initializers.glorot_normal(seed=seed)
+
+        model = keras.models.Sequential([
+
+            keras.layers.Dense(hidden_size, activation=keras.activations.selu, use_bias=True,
+                               kernel_initializer=initializer, input_shape=(input_size,)),
+            keras.layers.AlphaDropout(0.2, seed=seed),
+            # output layer
+            keras.layers.Dense(output_size, activation=keras.activations.softmax, use_bias=False,
+                               kernel_initializer=initializer)
+        ])
+
+    elif data == "cifar10":
+        input_size = 3072
+        hidden_size = 100
+        output_size = 10
+
+        if weights_hidden_size is not None:
+            hidden_size = weights_hidden_size
+
+        initializer = keras.initializers.glorot_normal(seed=seed)
+
+        model = keras.models.Sequential([
+
+            keras.layers.Dense(hidden_size, activation=keras.activations.selu, use_bias=True,
+                               kernel_initializer=initializer, input_shape=(input_size,)),
+
+            keras.layers.Dense(hidden_size, activation=keras.activations.selu, use_bias=True,
+                               kernel_initializer=initializer),
+
+            keras.layers.Dense(hidden_size, activation=keras.activations.selu, use_bias=True,
+                               kernel_initializer=initializer),
+            # output layer
+            keras.layers.Dense(output_size, activation=keras.activations.selu, use_bias=False,
+                               kernel_initializer=initializer)
+        ])
+
+    else:
+        raise Exception("wrong dataset")
+
+    adam = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, amsgrad=False)
+    model.compile(optimizer=adam, loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy', 'sparse_categorical_crossentropy'])
+
+    return model
+
+
+def crossover_offspring(data, x_train, y_train, x_test, y_test, pair_list, work_id, data_struc,
+                        parallel="process"):
+    # shuffle input data here
+    # np.random.seed(work_id)
+    # shuffle_list = np.arange(x_train.shape[0])
+    # np.random.shuffle(shuffle_list)
+    # x_train = x_train[shuffle_list]
+    # y_train = y_train[shuffle_list]
+
+    num_pairs = len(pair_list)
+    pair_id = work_id
+
+    print("FOR PAIR NUMBER " + str(pair_id + 1))
+
+    # crossover_types = ["safe", "unsafe", "orthogonal", "normed", "naive", "noise_0.5", "noise_0.1",
+    #"noise_low_corr"]
+    crossover_types = ["safe"]
+
+    result_list = [[] for _ in range(len(crossover_types))]
+    similarity_list = [[] for _ in range(len(crossover_types))]
+    quantile = 0.5
+    total_training_epoch = 4
+    epoch_list = [2]
+
+    model_one = model_keras(pair_id, data)
+    model_two = model_keras(pair_id + num_pairs, data)
+    model_one.save("parent_one_initial")
+    model_two.save("parent_two_initial")
+
+    print("one")
+    save_callback = CustomSaver(epoch_list, "parent_one")
+    model_information_parent_one = model_one.fit(x_train, y_train, epochs=total_training_epoch, batch_size=256,
+                                                 verbose=False,
+                                                 validation_data=(x_test, y_test), callbacks=[save_callback])
+    print("two")
+
+    save_callback = CustomSaver(epoch_list, "parent_two")
+    model_information_parent_two = model_two.fit(x_train, y_train, epochs=total_training_epoch, batch_size=256,
+                                                 verbose=False,
+                                                 validation_data=(x_test, y_test), callbacks=[save_callback])
+    print("three")
+
+    accuracy_best_parent = model_information_parent_one.history["val_loss"]
+    if np.max(accuracy_best_parent) < np.max(model_information_parent_two.history["val_loss"]):
+        accuracy_best_parent = model_information_parent_two.history["val_loss"]
+
+    for crossover_time in epoch_list:
+        count = 0
+        for crossover in crossover_types:
+
+            # get the parent weights at the corresponding epoch
+            parent_one = load_model("model_parent_one_epoch" + str(crossover_time) + ".hd5")
+            weights_nn_one = parent_one.get_weights()
+
+            parent_two = load_model("model_parent_two_epoch" + str(crossover_time) + ".hd5")
+            weights_nn_two = parent_two.get_weights()
+
+            print("crossover method: " + crossover)
+            list_ordered_weights_one, list_ordered_weights_two = weights_nn_one, weights_nn_two
+            list_hidden_representation_one = get_hidden_layers(parent_one, x_test)
+            list_hidden_representation_two = get_hidden_layers(parent_two, x_test)
+            list_corr_matrices = get_corr(list_hidden_representation_one, list_hidden_representation_two)
+
+            if crossover in ["safe", "unsafe", "orthogonal", "normed", "naive"]:
+                list_ordered_weights_one, list_ordered_weights_two, parents_similarity = crossover_method(
+                    list_ordered_weights_one, list_ordered_weights_two, list_corr_matrices, crossover)
+
+            print("seven")
+            if crossover in ["noise_0.5", "noise_0.1"]:
+                weights_crossover = add_noise_to_fittest(list_ordered_weights_one, list_ordered_weights_two,
+                                                         model_information_parent_one, model_information_parent_two,
+                                                         crossover,
+                                                         pair_id, crossover_time)
+
+            elif crossover in ["noise_low_corr"]:
+                weights_crossover = low_corr_neurons(list_ordered_weights_one, list_ordered_weights_two,
+                                                     list_corr_matrices, model_information_parent_one,
+                                                     model_information_parent_two, pair_id, crossover_time,
+                                                     crossover, quantile)
+            else:
+                weights_crossover = arithmetic_crossover(list_ordered_weights_one, list_ordered_weights_two)
+
+            model_offspring = model_keras(0, data, weights_crossover[0].shape[1])
+
+            model_offspring.set_weights(weights_crossover)
+            model_information_offspring = model_offspring.fit(x_train, y_train,
+                                                              epochs=total_training_epoch - crossover_time,
+                                                              batch_size=256,
+                                                              verbose=False, validation_data=(x_test, y_test))
+
+            result_list[count].append(accuracy_best_parent)
+            result_list[count].append(model_information_offspring.history["val_loss"])
+
+            if crossover not in ["noise_0.5", "noise_0.1", "noise_low_corr"]:
+                similarity_list[count].append(parents_similarity)
+
+            keras.backend.clear_session()
+            count += 1
+
+    if parallel == "process":
+        data_struc[str(work_id) + "_performance"] = result_list
+        data_struc[str(work_id) + "_similarity"] = similarity_list
+    elif parallel == "thread":
+        data_struc.put(result_list)
+
+    print("ten")
+
+
+if __name__ == "__main__":
+
+    data = "mnist"
+
+    if data == "cifar10":
+        x_train, x_test, y_train, y_test = load_cifar()
+    elif data == "mnist":
+        x_train, x_test, y_train, y_test = load_mnist()
+
+    parallel_method = "process"
+
+    if parallel_method == "thread":
+        num_threads = 1
+
+        start = timer()
+
+        q = queue.Queue()
+
+        pair_list = [pair for pair in range(num_threads)]
+
+        t = [threading.Thread(target=crossover_offspring, args=(data, x_train, y_train, x_test, y_test,
+                                                                pair_list, i, q, parallel_method)) for i in
+             range(num_threads)]
+
+        for thread in t:
+            thread.start()
+
+        results = [q.get() for _ in range(num_threads)]
+
+        # Stop these threads
+        for thread in t:
+            thread.stop = True
+
+        end = timer()
+        print(end - start)
+
+    elif parallel_method == "process":
+        num_processes = 1
+
+        start = timer()
+
+        manager = multiprocessing.Manager()
+        return_dict = manager.dict()
+
+        pair_list = [pair for pair in range(num_processes)]
+
+        p = [multiprocessing.Process(target=crossover_offspring, args=(data, x_train, y_train, x_test, y_test,
+                                                                       pair_list, i, return_dict,
+                                                                       parallel_method)) for i in range(num_processes)]
+
+        for proc in p:
+            proc.start()
+        for proc in p:
+            proc.join()
+
+        results = return_dict.values
+
+        end = timer()
+        print(end - start)
---
+++ utils.py
@@ -0,0 +1,294 @@
+import numpy as np
+from scipy.optimize import linear_sum_assignment
+from sklearn.preprocessing import StandardScaler
+import pickle
+import keras
+
+
+# load data functions
+def unpickle(file):
+    with open(file, 'rb') as fo:
+        data_dict = pickle.load(fo, encoding='bytes')
+    return data_dict
+
+
+def load_mnist():
+    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+
+    x_train = np.reshape(x_train, (x_train.shape[0], 28 * 28))
+    x_test = np.reshape(x_test, (x_test.shape[0], 28 * 28))
+
+    return x_train, x_test, y_train, y_test
+
+
+def load_cifar(flatten=True):
+    (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+
+    x_train = x_train / 255.0
+    x_test = x_test / 255.0
+
+    if flatten:
+        x_train = np.reshape(x_train, (x_train.shape[0], 3072))
+        x_test = np.reshape(x_test, (x_test.shape[0], 3072))
+
+    return x_train, x_test, y_train, y_test
+
+
+def selu(x, lamb=1.0507, alpha=1.67326):
+    x[x > 0] = x[x > 0] * lamb
+    x[x <= 0] = lamb * (alpha * np.exp(x[x <= 0]) - alpha)
+
+    return x
+
+
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+
+    return e_x / e_x.sum()
+
+
+def add_noise(parent_weights, t, seed):
+    np.random.seed(seed)
+
+    scale_factor = np.sqrt(1 / (np.power(t, 2) + np.power(1 - t, 2)))
+    mean_parent, std_parent = 0.0, np.std(parent_weights)
+
+    weight_noise = np.random.normal(loc=mean_parent, scale=std_parent, size=parent_weights.shape)
+    parent_weights = (t * parent_weights + (1 - t) * weight_noise) * scale_factor
+
+    return parent_weights
+
+
+def get_hidden_layers(model, data_x):
+
+    def keras_function_layer(model_layer, data):
+        hidden_func = keras.backend.function(model.layers[0].input, model_layer.output)
+        result = hidden_func([data])
+
+        return result
+
+    hidden_layers_list = []
+    for index in range(len(model.layers)-1):
+        hidden_layer = keras_function_layer(model.layers[index], data_x)
+        hidden_layers_list.append(hidden_layer)
+
+    return hidden_layers_list
+
+
+def get_corr(hidden_representation_list_one, hidden_representation_list_two):
+    list_corr_matrices = []
+
+    for index in range(len(hidden_representation_list_one)):
+        hidden_representation_one = hidden_representation_list_one[index]
+        hidden_representation_two = hidden_representation_list_two[index]
+
+        n = hidden_representation_one.shape[1]
+        scaler = StandardScaler()  # Fit your data on the scaler object
+        hidden_representation_one = scaler.fit_transform(hidden_representation_one)
+        hidden_representation_two = scaler.fit_transform(hidden_representation_two)
+
+        corr_matrix_nn = np.empty((n, n))
+
+        for i in range(n):
+            for j in range(n):
+                corr = np.corrcoef(hidden_representation_one[:, i], hidden_representation_two[:, j])[0, 1]
+                corr_matrix_nn[i, j] = corr
+
+        list_corr_matrices.append(corr_matrix_nn)
+
+    return list_corr_matrices
+
+
+# cross correlation function for both bipartite matching (hungarian method)
+def bipartite_matching(corr_matrix_nn, crossover="unsafe"):
+    if crossover == "unsafe":
+        list_neurons_x, list_neurons_y = linear_sum_assignment(corr_matrix_nn)  # Hungarian method
+    elif crossover == "safe":
+        corr_matrix_nn *= -1  # default of linear_sum_assignement is to minimize cost, we want to max "cost"
+        list_neurons_x, list_neurons_y = linear_sum_assignment(corr_matrix_nn)  # Hungarian method
+    elif crossover == "orthogonal":
+        corr_matrix_nn = np.abs(corr_matrix_nn)
+        list_neurons_x, list_neurons_y = linear_sum_assignment(corr_matrix_nn)  # Hungarian method
+    elif crossover == "normed":
+        corr_matrix_nn = np.abs(corr_matrix_nn)
+        corr_matrix_nn *= -1
+        list_neurons_x, list_neurons_y = linear_sum_assignment(corr_matrix_nn)  # Hungarian method
+    elif crossover == "naive":
+        list_neurons_x, list_neurons_y = list(range(corr_matrix_nn.shape[0])), list(range(corr_matrix_nn.shape[0]))
+    else:
+        raise ValueError('the crossover method is not defined')
+
+    return list_neurons_x, list_neurons_y
+
+
+def get_network_similarity(list_corr_matrices, list_ordered_indices_one, list_ordered_indices_two):
+    list_meta = []
+
+    for layer_num in range(len(list_corr_matrices)):
+        list_corr = []
+        for index in range(len(list_ordered_indices_one)):
+            i = list_ordered_indices_one[layer_num][index]
+            j = list_ordered_indices_two[layer_num][index]
+            corr = np.abs(list_corr_matrices[layer_num][i][j])
+            list_corr.append(corr)
+
+        list_meta.append(np.mean(list_corr))
+
+    similarity = np.mean(list_meta)
+
+    return similarity
+
+
+# Algorithm 2
+def apply_mask_to_weights(nn_weights_list, list_indices_hidden):
+    count = 0
+    depth = count * 2
+    for layer in range(len(list_indices_hidden)):
+        for index in range(3):
+            if index == 0:
+                # order columns for weights
+                nn_weights_list[index + depth] = nn_weights_list[index + depth][:, list_indices_hidden[layer]]
+            elif index == 1:
+                nn_weights_list[index + depth] = nn_weights_list[index + depth][
+                    list_indices_hidden[layer]]  # order columns for bias
+            elif index == 2:
+                # order rows
+                nn_weights_list[index + depth] = nn_weights_list[index + depth][list_indices_hidden[layer], :]
+
+        count += 1
+        depth = count * 2
+
+    nn_weights_list = np.asarray(nn_weights_list)
+
+    return nn_weights_list
+
+
+def apply_mask_and_add_noise(nn_weights_list, list_mask, seed):
+
+    count = 0
+    depth = count * 2
+    for layer in range(len(list_mask)):
+        for index in range(3):
+            if index == 0:
+                # noise the columns
+                nn_weights_list[index + depth][:, list_mask[layer]] = \
+                    add_noise(nn_weights_list[index + depth][:, list_mask[layer]], 0.5, seed)
+            elif index == 1:
+                # order columns for bias
+                nn_weights_list[index + depth][list_mask[layer]] = \
+                    add_noise(nn_weights_list[index + depth][list_mask[layer]], 0.5, seed)
+            elif index == 2:
+                # order rows
+                nn_weights_list[index + depth][list_mask[layer], :] = \
+                    add_noise(nn_weights_list[index + depth][list_mask[layer], :], 0.5, seed)
+
+        count += 1
+        depth = count * 2
+
+    nn_weights_list = np.asarray(nn_weights_list)
+
+    return nn_weights_list
+
+
+def crossover_method(weights_one, weights_two, list_corr_matrices, crossover):
+    list_ordered_indices_one = []
+    list_ordered_indices_two = []
+    for index in range(len(list_corr_matrices)):
+        corr_matrix_nn = list_corr_matrices[index]
+
+        indices_one, indices_two = bipartite_matching(corr_matrix_nn, crossover)
+        list_ordered_indices_one.append(indices_one)
+        list_ordered_indices_two.append(indices_two)
+
+    similarity = get_network_similarity(list_corr_matrices, list_ordered_indices_one, list_ordered_indices_two)
+
+    # order the weight matrices
+
+    if crossover == "naive":
+        list_ordered_w_one = list(weights_one)
+        list_ordered_w_two = list(weights_two)
+
+    else:
+        weights_nn_one_copy = list(weights_one)
+        weights_nn_two_copy = list(weights_two)
+        list_ordered_w_one = apply_mask_to_weights(weights_nn_one_copy, list_ordered_indices_one)
+        list_ordered_w_two = apply_mask_to_weights(weights_nn_two_copy, list_ordered_indices_two)
+
+    return list_ordered_w_one, list_ordered_w_two, similarity
+
+
+def arithmetic_crossover(network_one, network_two, t=0.5):
+    scale_factor = np.sqrt(1 / (np.power(t, 2) + np.power(1 - t, 2)))
+
+    list_weights = []
+    for index in range(len(network_one)):
+        averaged_weights = (t * network_one[index] + (1 - t) * network_two[index]) * scale_factor
+        list_weights.append(averaged_weights)
+
+    return list_weights
+
+
+def add_noise_to_fittest(network_one, network_two, information_nn_one, information_nn_two, crossover, seed, index):
+
+    t = 0.5
+    if crossover == "noise_0.1":
+        t = 0.9
+
+    # choose best parent
+    best_parent = network_one
+    if index == 0:
+        pass
+    else:
+        if np.max(information_nn_one.history["val_loss"]) < np.max(information_nn_two.history["val_loss"]):
+            best_parent = network_two
+
+    list_weights = []
+    for index in range(len(best_parent)):
+        parent_weights = best_parent[index]
+        parent_weights = add_noise(parent_weights, t, seed)
+
+        list_weights.append(parent_weights)
+
+    return list_weights
+
+
+def low_corr_neurons(network_one, network_two, corr_matrices_list, information_nn_one, information_nn_two,
+                     seed, index, crossover, threshold):
+
+    # choose best parent
+    best_parent = network_one
+    best_parent_switch = False
+    if index == 0:
+        pass
+    else:
+        if np.max(information_nn_one.history["val_loss"]) < np.max(information_nn_two.history["val_loss"]):
+            best_parent = network_two
+            best_parent_switch = True
+
+    mask_list = []
+    for corr_matrix in corr_matrices_list:
+        max_corr_list = []
+        for i in range(corr_matrix.shape[0]):
+            list_corr = []
+            for j in range(corr_matrix.shape[1]):
+                if best_parent_switch:
+                    corr = np.abs(corr_matrix[i, j])
+                else:
+                    corr = np.abs(corr_matrix[j, i])
+
+                list_corr.append(corr)
+            max_corr_list.append(np.max(list_corr))
+
+        quantile = np.quantile(max_corr_list, threshold)
+        mask_array = np.asarray(max_corr_list) >= quantile
+
+        mask_list.append(mask_array)
+
+    if crossover == "noise_low_corr":
+        mask_list = [[not element for element in sublist] for sublist in mask_list]
+        list_weights = apply_mask_and_add_noise(best_parent, mask_list, seed)
+
+        return list_weights
